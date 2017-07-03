03e299b835df4fbe0d2448f5dc15de216fb7fbcb
Oscar Rosas <SoyOscarRH@gmail.com>
2017-07-03 16:27:31 -0500

    Go, you're almost complete

--- Math/Euclidean-GCD-LCM.cpp
+++ Math/Euclidean-GCD-LCM.cpp
@@ -14,6 +14,10 @@ typedef long long lli;                                                      //Ju
 
 
 
+
+/*=================================================================
+=============   JUST TO LEARN AND SEE STEP BY STEP   ==============
+=================================================================*/
 // ******************* LONG DIVISION ALGORITHM ************************
 pair<lli, lli> LongDivisionAlgorithm(lli a, lli b, bool ShowIt){            //FN: Return the only q and r 
     lli q, r;                                                               //Variables, remember [0,|b|) in r
@@ -23,7 +27,7 @@ pair<lli, lli> LongDivisionAlgorithm(lli a, lli b, bool ShowIt){            //FN
         r = a%b;                                                            //Get me the reminder
 
         if (r < 0){                                                         //If we have a problem
-            (q > 0)? q++: q--;                                              //If q>0 add 1, else sub 1
+            (q >= 0)? q++: q--;                                             //If q>0 add 1, else sub 1
             r = a - (b*q);                                                  //Recalculate reminder
         }
     }
@@ -38,31 +42,101 @@ pair<lli, lli> LongDivisionAlgorithm(lli a, lli b, bool ShowIt){            //FN
 }
 
 
-
 // ******************* EUCLIDEAN ALGORITHM ****************************
 vector< vector<lli> > EuclideanAlgorithm(lli a, lli b, bool ShowIt){        //FN: Return data for each step
     vector< vector<lli> > Data;                                             //Step by step Data[i]={a,b,q,r}    
-    lli r, q;                                                               //The last 2 variables for algorithm 
+    lli RealA = a, RealB = b, r, q;                                         //The last 2 variables for algorithm 
     bool SpecialCase = false;                                               //This is a special case              
     if (b == 0) SpecialCase = true;                                         //Just activate this flag
 
     do {                                                                    //Do this at least one time
-        auto Step = LongDivisionAlgorithm(a,b, false);                      //Get the long division
+        auto Step = LongDivisionAlgorithm(a,b,false);                       //Get the long division
         q = Step.first;                                                     //Get values
         r = Step.second;                                                    //Get values
 
         Data.push_back({a,b,q,r});                                          //Add the result to the data
+        if (SpecialCase) break;                                             //Break if we are over
 
         a = b;                                                              //The new a is b
         b = r;                                                              //The new b is r
     }
-    while (b != 0&& !SpecialCase);                                          //Do it! while we can
+    while (b != 0);                                                         //Do it! while we can
 
-    if (ShowIt){                                                            //You want to see it?
+    if (ShowIt) {                                                           //You want to see it?
+        cout << "=== Process: ===\n";                                       //Title
         for (auto x : Data) {                                               //For each element in the data
-            cout << "(a:"<<x[0] << ") = (b:"<<x[1]<<")";                    //Show it!
-            cout << "(q:"<<x[2] << ") + (r:"<<x[3]<<") \n";                 //Show it!
+            cout << "(a:"<< x[0] << ") = (b:" << x[1] << ")";               //Show it!
+            cout << "(q:"<< x[2] << ") + (r:" << x[3] << ")\n";             //Show it!
         }
+        cout << "\nSo GCD("<<RealA<<", "<<RealB<< ") = "<<abs(a)<<"\n";     //You maybe want to know this
+    }
+
+    return Data;                                                            //Return the Data
+}
+
+
+
+// *******************EXTENDED EUCLIDEAN ALGORITHM ********************
+vector< vector<lli> > ExtendedEuclideanAlgorithm(lli a,lli b,bool ShowIt){  //FN: Return data for each step
+    vector< vector<lli> > Data;                                             //Step by step Data[i]={a,b,q,r,x,y}    
+    lli r, q, Temporal;                                                     //Variables for algorithm 
+    lli RealA = a, RealB = b, LastM = 1, LastN = 0, m = 0, n = 1;           //Variables for Bezut Identity
+    
+    bool SpecialCase = false;                                               //This is a special case              
+    if (b == 0) SpecialCase = true;                                         //Just activate this flag
+
+    do {                                                                    //Do this at least one time
+        auto Step = LongDivisionAlgorithm(a,b,false);                       //Get the long division
+        q = Step.first;                                                     //Get values
+        r = Step.second;                                                    //Get values
+
+        if (SpecialCase) {Data.push_back({a, b, q, r, 1, 0}); break;}       //If b=0, original info is all we need
+
+        Temporal = m;                                                       //Lets save m
+        m = LastM - m*q;                                                    //Lets create the new m as lastm - lastm*q;
+        LastM = Temporal;                                                   //Now you are the last m 
+
+        Temporal = n;                                                       //Lets save n
+        n = LastN - n*q;                                                    //Lets create the new n as lastn - lastn*q;
+        LastN = Temporal;                                                   //Now you are the last n 
+ 
+        Data.push_back({a, b, q, r, m, n});                                 //Add the result to the data
+        a = b;                                                              //The new a is b
+        b = r;                                                              //The new b is r
+    }
+    while (b != 0);                                                         //Do it! while we can
+
+    if (ShowIt) {                                                           //Nou want to see it?
+
+        cout << "=== Process for GCD: ===\n";                               //The title
+        for (auto x : Data) {                                               //For each element in the data
+            cout << "(a:"<<x[0]<< ") = (b:"<<x[1]<<")";                     //Show it!
+            cout << "(q:"<<x[2]<< ") + (r:"<<x[3]<<") \n";                  //Show it!
+        }
+
+        cout << "\n\n=== Process for Bezut Coeficients: ===\n";             //The title for the Bezut Process
+        cout << "(a':"<<RealA<< ") = " << "(a':"<<RealA<< ")(m:1) + ";      //The original lineal combination of a
+        cout << "(b':"<<RealB<< ")(n:0)\n";                                 //The original lineal combination of a
+
+        cout << "(b':"<<RealB<< ") = " << "(a':"<<RealA<< ")(m:0) + ";      //The original lineal combination of b
+        cout << "(b':"<<RealB<< ")(n:1)\n\n";                               //The original lineal combination of b
+
+        for (auto x : Data) {                                               //For each element in the data
+            cout << "(r:"<<x[3]<<") = " << "(a:"<<x[0]<<") - ";             //Show it (r) = (a) - (b)(q)
+            cout << "(b:"<<x[1]<<")" << "(1:"<<x[2]<<")  =  ";              //Show it (r) = (a) - (b)(q)
+
+            cout << "(a':"<<RealA<<")(m:"<<x[4]<<") + ";                    //Show it!
+            cout << "(b':"<<RealB<<")(n:"<<x[5]<<") \n";                    //Show it!
+        } 
+
+        cout << "\nSo GCD("<<RealA<<", "<<RealB<< ") = "<<abs(a)<<"\n";     //You maybe want to know this
+        
+        cout << "So BezutNumbers("<< RealA <<", "<< RealB << ") = ";        //You maybe want to know this
+        cout << "("<< LastM <<", "<< LastN <<")\n";                         //You maybe want to know this
+        
+        cout << "So Bezut Indentity: (GCD:"<< abs(a) << ") = ";             //You maybe want to know this
+        cout << "(a':" << RealA << ")(m:" << LastM << ") +";                //You maybe want to know this
+        cout << "(b':" << RealB << ")(n:" << LastN << ") \n";               //You maybe want to know this
     }
 
     return Data;                                                            //Return the Data
@@ -71,26 +145,69 @@ vector< vector<lli> > EuclideanAlgorithm(lli a, lli b, bool ShowIt){        //FN
 
 
 
+/*=================================================================
+========  BEST OPTIMIZATIONS OF THIS FOR REAL LIFE      ===========
+=================================================================*/
+
+// ******************* LONG DIVISION ALGORITHM ************************
+pair<lli, lli> LongDivisionAlgorithm(lli a, lli b){                         //FN: Return the only q and r 
+    lli q, r;                                                               //Variables, remember [0,|b|) in r
+
+    if (b != 0) {                                                           //If we have work to do
+        q = a/b;                                                            //Get me the floor of this
+        r = a%b;                                                            //Get me the reminder
+
+        if (r < 0){                                                         //If we have a problem
+            (q >= 0)? q++: q--;                                             //If q>0 add 1, else sub 1
+            r = a - (b*q);                                                  //Recalculate reminder
+        }
+    }
+    else {q = 0; r = a;}                                                    //b=0, this is always the result
+    return {q,r};                                                           //Return the info
+}
+
+
 // ******************* GREAT COMMON DIVIDER: EUCLIDEAN EDITION  **********
-ull GCD(lli numberA, lli numberB){                                          //FN: Return Great Common Divider of 2 #
+ull GCD(lli a, lli b){                                                      //FN: Return GreatCommonDivider of 2 #
     lli reminder;                                                           //Lets create a reminder
 
-    while(numberB != 0){                                                    //Rembember GCD(A,0) = |A|
-        reminder = numberA % numberB;                                       //Get me the reminder of the 2 numbers
-        numberA = numberB;                                                  //Know A=BQ+R -> GCD(A,B) = GCD(B,R)
-        numberB = reminder;                                                 //Let's calculate GCD(B,R)
+    while(b != 0){                                                          //Rembember GCD(A,0) = |A|
+        reminder = a % b;                                                   //Get me the reminder of the 2 numbers
+        a = b;                                                              //Know A=BQ+R -> GCD(A,B) = GCD(B,R)
+        b = reminder;                                                       //Let's calculate GCD(B,R)
     }
 
-    return abs(numberA);                                                    //Get me the A when B is 0 GCD(A,0)=|A|
+    return abs(a);                                                          //Get me the A when B is 0 GCD(A,0)=|A|
 }
 
 // ******************* LEAST COMMON MULTIPLE *****************************
-ull LCM(lli numberA, lli numberB){                                          //FN: Return the LCM of 2 numbers     
-    if (numberA > 0) numberA *= -1;                                         //Just ignore the negatives
-    if (numberB > 0) numberB *= -1;                                         //Just ignore the negatives
-    return (numberB * numberA) / GCD(numberA, numberB);                     //THEOREM: LCM(a, b) = (a x b) / GCD(a,b)
+ull LCM(lli a, lli b){                                                      //FN: Return the LCM of 2 numbers 
+    return (abs(a) * abs(b)) / GCD(a, b);                                   //THEOREM: LCM(a, b) = (a x b) / GCD(a,b)
 }
 
+// ******************* GREAT COMMON DIVIDER: EUCLIDEAN EDITION  **********
+pair<lli, lli> BezutCoefficients(lli a, lli b){                             //FN: Return GreatCommonDivider of 2 #
+    lli Temporal, q, r, m = 0, n = 1, LastM = 1, LastN = 0;                 //The variables
+
+    while(b != 0){                                                          //Rembember GCD(A,0) = |A|
+        auto Step = LongDivisionAlgorithm(a,b);                             //Get the long division
+        q = Step.first;                                                     //Get values
+        r = Step.second;                                                    //Get values
+        
+        a = b;                                                              //Know A=BQ+R -> GCD(A,B) = GCD(B,R)
+        b = r;                                                              //Let's calculate GCD(B,R)
+
+        Temporal = m;                                                       //Lets save m
+        m = LastM - m*q;                                                    //Lets create the new m as lastm - lastm*q;
+        LastM = Temporal;                                                   //Now you are the last m 
+
+        Temporal = n;                                                       //Lets save n
+        n = LastN - n*q;                                                    //Lets create the new n as lastn - lastn*q;
+        LastN = Temporal;                                                   //Now you are the last n 
+    }
+
+    return {LastM, LastN};                                                  //Get me the last 2 variables
+}
 
 
 
@@ -101,15 +218,16 @@ ull LCM(lli numberA, lli numberB){                                          //FN
 // ==============================================
 int main(){
 
-    lli CasualNumber;                                                   //Let's create a Casual Number
-
     // ====== SECTION: LONG DIVISION =============  
-    //LongDivisionAlgorithm(-8,0,true);
+    //LongDivisionAlgorithm(-1,-3,true);
     
     
     // ====== SECTION: EUCLIDEAN ALGORITHM  ======
-    auto Data = EuclideanAlgorithm(2024, 748, true);
+    //EuclideanAlgorithm(141, 96, true);                                  //Remember GCD is Data[Size-1][3] or is obvious 
+    //ExtendedEuclideanAlgorithm(141, 96, true);                          //gcd=Data[S-1][3], m=Data[S-1][4] m=Data[S-1][5]
 
+    //auto Data = BezutCoefficients(141, 96);                             //Get you the pair of m and n 
+    //cout << "m = "<<Data.first<< " n = "<<Data.second<< "\n";           //Show it
 
     // ====== SECTION: APLICATIONS GCD / LCM  ====
     //cout << "GCD is " << GCD(-5,17) << endl;                            //Show it!
