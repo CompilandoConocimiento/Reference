9eade85b277cecbe4b2c949c89d6b928f4efb818
Oscar Andrés Rosas <soyoscarrh@gmail.com>
2018-06-03 22:06:49 -0500

    Add comments to Knapsack

--- C++/DynamicProgramming/Knapsack.cpp
+++ C++/DynamicProgramming/Knapsack.cpp
@@ -4,60 +4,48 @@
 
 https://www.geeksforgeeks.org/knapsack-problem/                 */
 
-#include <iostream>                                                     
-#include <vector>                                                       
-using namespace std;                                                    
+#include <iostream>                                                                         //Include library
+#include <vector>                                                                           //Include library
+using namespace std;                                                                        //Bad habit
 
-int Knapsack(const vector<int>& Values, const vector<int>& Costs, int CapacityInCost) { 
-    
-    int NumberOfItems = Values.size();
-    int DataStore[NumberOfItems + 1][CapacityInCost + 1];
+int Knapsack (vector<int>& Values, vector<int>& Costs, int CapacityInCost) {                //Parameters to fn
 
-    for (int item = 0; item <= NumberOfItems; ++item) {
-        
-        for (int actualCost = 0; actualCost <= CapacityInCost; ++actualCost) {
+    int NumberOfItems = Values.size();                                                      //Number of items
+    int DataStore[NumberOfItems + 1][CapacityInCost + 1];                                   //DP Table
+
+    for (int item = 0; item <= NumberOfItems; ++item) {                                     //Foreach item number
+        for (int actualCost = 0; actualCost <= CapacityInCost; ++actualCost) {              //For each possible cap
             
-            int CostOfPreviousItem = Costs[item - 1];
-            int ValueOfPreviousItem = Values[item - 1];
-            int CostUntilPreviousItem = DataStore[item - 1][actualCost];
+            int CostOfPreviousItem = Costs[item - 1];                                       //Temporal vars
+            int ValueOfPreviousItem = Values[item - 1];                                     //Temporal vars
+            int CostUntilPrevItem = DataStore[item - 1][actualCost];                        //Temporal vars
 
-            if (item == 0 or actualCost == 0) {
-                DataStore[item][actualCost] = 0;
-                continue;
+            if (item == 0 or actualCost == 0) {                                             //If at the start
+                DataStore[item][actualCost] = 0;                                            //Put zero
             }
-            else if (CostOfPreviousItem > actualCost) {
-               DataStore[item][actualCost] = CostUntilPreviousItem;
-               continue;
+            else if (CostOfPreviousItem > actualCost) {                                     //If is better
+               DataStore[item][actualCost] = CostUntilPrevItem;                             //take it
             }
-            else {
-                int RemoveLastItem = DataStore[item - 1][actualCost - CostOfPreviousItem];
-                int NewOption = max(CostUntilPreviousItem, RemoveLastItem + ValueOfPreviousItem);
-                DataStore[item][actualCost] = NewOption;
+            else {                                                                          //If is worst
+                int RemoveLastItem = DataStore[item - 1][actualCost - CostOfPreviousItem];  //Remove the last item
+                int NewOption = max(CostUntilPrevItem, RemoveLastItem+ValueOfPreviousItem); //Get the better deal
+                DataStore[item][actualCost] = NewOption;                                    //New options
             }
         }
     }
 
-    return DataStore[NumberOfItems][CapacityInCost];
+    return DataStore[NumberOfItems][CapacityInCost];                                        //Return the result
 }
 
 
 int main() {
 
     int NumberOfItems, CapacityInCost;
-    cin >> NumberOfItems >> CapacityInCost;
+    cin >> CapacityInCost >> NumberOfItems;
 
     vector<int> Values(NumberOfItems), Costs(NumberOfItems);
-
-    for (int i = 0, temporal; i < NumberOfItems; ++i) {
-        cin >> temporal;
-        Values[i] = temporal;
-    }
-
-    for (int i = 0, temporal; i < NumberOfItems; ++i) {
-        cin >> temporal;
-        Costs[i] = temporal;
-    }
-
+    for (int i = 0; i < NumberOfItems; ++i) cin >> Costs[i] >> Values[i];
+    
     cout << Knapsack(Values, Costs, CapacityInCost) << "\n";
 
     return 0;
