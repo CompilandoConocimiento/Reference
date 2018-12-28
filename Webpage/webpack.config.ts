acbc0bb8b003de643d2e0227500c15085b5d7438
Oscar Andrés Rosas <soyoscarrh@gmail.com>
2018-12-27 21:02:42 -0600

    Fix

--- Webpage/webpack.config.ts
+++ Webpage/webpack.config.ts
@@ -1,50 +1,50 @@
 import {Configuration} from 'webpack'
 
 const Configuration: Configuration = {
-	entry: './Code/App/App.tsx',
-	output: {
-		path: __dirname + '/Distribution/',
-		publicPath: "Webpage/Distribution/",
-		filename: 'bundle.js'
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.jsx', '.js', '.css', ]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx|tsx|ts)$/,
-				exclude: /node_modules/,
-				use: 'babel-loader'
-			},
-			{
-				test: /\.(png|jpg|gif)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {}  
-					}
-				]
-			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					{
-						loader: 'typings-for-css-modules-loader?&namedExport&camelCase',
-						options: {
-							modules: true,
-							camelCase: true,
-							namedExport: true,
-							localIdentName: "[name]_[local]_[hash:base64]",
-							sourceMap: true,
-							minimize: true
-						}
-					}
-				]
-			}
-		]
-	}
+    entry: './Code/App/App.tsx',
+    output: {
+        path: __dirname + '/Distribution/',
+        publicPath: "Webpage/Distribution/",
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '.css', ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|tsx|ts)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}  
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'typings-for-css-modules-loader?&namedExport&camelCase',
+                        options: {
+                            modules: true,
+                            camelCase: true,
+                            namedExport: true,
+                            localIdentName: "[name]_[local]_[hash:base64]",
+                            sourceMap: true,
+                            minimize: true
+                        }
+                    }
+                ]
+            }
+        ]
+    }
 }
 
 module.exports = Configuration
