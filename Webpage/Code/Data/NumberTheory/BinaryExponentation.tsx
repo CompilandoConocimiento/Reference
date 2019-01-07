054f0e5a12d7e8fe59039346b53e459295499646
Oscar Andrés Rosas <soyoscarrh@gmail.com>
2019-01-06 21:15:36 -0600

    Update to build

--- Webpage/Code/Data/NumberTheory/BinaryExponentation.tsx
+++ Webpage/Code/Data/NumberTheory/BinaryExponentation.tsx
@@ -21,86 +21,116 @@ const PageConfig: AlgorithmPageInformation = {
 const BinaryExponentation: React.FunctionComponent<AlgorithmPageProps> = ({filesData}) => {
     return (
         <div>
-            This function will performe \( {"base^{exponent}"} \) but a little bit more fast using
-            exponentiation by squaring.
-            The idea is to see this way of expressing exponentiation:
-            <LaTeX>
-                {String.raw`
-                    base ^ {exponent} = 
-                    \begin{cases}
-                    (base^2)^{\; \frac{exponent}{2}}             & \text{if exponent is even}  \\
-                    base \; (base^2)^{\; \frac{exponent-1}{2}}   & \text{if exponent is odd}   \\
-                    \end{cases}
-                `}  
-            </LaTeX>
 
-            Or if you like small variable names we can say that \( base = x \) y \( exponent = n \):
-            <LaTeX>
-                {String.raw`
-                    x^n = 
-                    \begin{cases}
-                    (x^2)^{\; \frac{n}{2}}           & \text{if n is even}  \\
-                    x \; (x^2)^{\; \frac{n-1}{2}}    & \text{if n is odd}   \\
-                    \end{cases}
-                `}  
-            </LaTeX>
+            <h5><b>Definition</b></h5>
 
-            <ShowCode 
-                Data       = {filesData}
-                fileName   = {"BinaryExponentiation.cpp"}
-                partOfFile = {"RecursiveBinExpo"}
-            />
-            <br />
+                This function will performe \( {"base^{exponent}"} \) but a little bit more fast using
+                exponentiation by squaring.
+                The idea is to see this way of expressing exponentiation:
+                <LaTeX>
+                    {String.raw`
+                        base ^ {exponent} = 
+                        \begin{cases}
+                        (base^2)^{\; \frac{exponent}{2}}             & \text{if exponent is even}  \\
+                        base \; (base^2)^{\; \frac{exponent-1}{2}}   & \text{if exponent is odd}   \\
+                        \end{cases}
+                    `}  
+                </LaTeX>
 
-            <h6><b>How to optimize \( {"e = \\frac{e-1}{2} "} \) and \( {"e = \\frac{e}{2} "}\) </b></h6>
+                Or if you like small variable names we can say that \( base = x \) y \( exponent = n \):
+                <LaTeX>
+                    {String.raw`
+                        x^n = 
+                        \begin{cases}
+                        (x^2)^{\; \frac{n}{2}}           & \text{if n is even}  \\
+                        x \; (x^2)^{\; \frac{n-1}{2}}    & \text{if n is odd}   \\
+                        \end{cases}
+                    `}  
+                </LaTeX>
 
-            Remember the bitwise operations equalities:
-            <ul className="browser-default">
-                <li> { "\\( x << y = x * 2^y \\)" } </li>
-                <li> { "\\( x >> y = \\left \\lfloor \\dfrac{x}{2^y} \\right \\rfloor \\)" } </li>
-            </ul>
+                <ShowCode 
+                    Data       = {filesData}
+                    fileName   = {"BinaryExponentiation.cpp"}
+                    partOfFile = {"RecursiveBinExpo"}
+                />
+                <br />
 
-            This optimizations are using the bitwise operations.
-            I claim that 
-            \( {"e = \\frac{e-1}{2} "} \) and \( {"e = \\frac{e}{2} "}\) are equal to
-            \( {"e = e >> 1"}\).
+            <h5><b>Simple Optimizations</b></h5>
 
-            <br />
-            <br />
+            Now we want to eliminate the recursion and modify a little bit the idea:
+                <ul className="browser-default">
+                    <li>
+                        If exponent is odd, first do \( {"solution = base * solution"} \),
+                        now, we have to delete 1 to the exponent, so the exponent is even, so
+                        we can just say that the base is \( {"base = base^2"} \) and that the
+                        exponent is just the half of the original exponent.
+                    </li>
+                    <li>
+                        If exponent is even, so
+                        we can just say that the base is \( {"base = base^2"} \) and that the
+                        exponent is just the half of the original exponent.
+                    </li>
+                </ul>
 
-            This is easy to proof, because \( {"e = e >> 1"}\) delete the least significant bit,
-            so the last bit does not really matter. 
-            So you find that at the end it does not matter if it is odd or even
-            this will always give you the correct answer.
+                <ShowCode 
+                    Data       = {filesData}
+                    fileName   = {"BinaryExponentiation.cpp"}
+                    partOfFile = {"SimpleBinExpo"}
+                />
+                <br />
+            
 
-            <br />
-            <br />
+            <h5><b>Optimizations</b></h5>
 
-            <h6><b>Check in the exponent is odd or even</b></h6>
+                <h6><b>How to optimize \( {"e = \\frac{e-1}{2} "} \) and \( {"e = \\frac{e}{2} "} \) </b></h6>
 
-            To do this just do exponent & 1
-            <br />
-                
-            For example the number \(000101101010\) if the exponent 
-            was odd then the last digit will be 1, so exponent & 1
-            give true only if the last digit was 1.
+                Remember the bitwise operations equalities:
+                <ul className="browser-default">
+                    <li> { "\\( x << y = x * 2^y \\)" } </li>
+                    <li> { "\\( x >> y = \\left \\lfloor \\dfrac{x}{2^y} \\right \\rfloor \\)" } </li>
+                </ul>
 
-            <ShowCode 
-                Data       = {filesData}
-                fileName   = {"BinaryExponentiation.cpp"}
-                partOfFile = {"BinExpo"}
-            />
-            <br />
+                This optimizations are using the bitwise operations.
+                I claim that 
+                \( {"e = \\frac{e-1}{2} "} \) and \( {"e = \\frac{e}{2} "}\) are equal to
+                \( {"e = e >> 1"}\).
 
-            <h6><b>Modular Exponentation</b></h6>
+                <br />
+                <br />
 
-            This is to do \( {"base^{exponent}\\mod n"}\)
+                This is easy to proof, because \( {"e = e >> 1"}\) delete the least significant bit,
+                so the last bit does not really matter. 
+                So you find that at the end it does not matter if it is odd or even
+                this will always give you the correct answer.
 
-            <ShowCode 
-                Data       = {filesData}
-                fileName   = {"BinaryExponentiation.cpp"}
-                partOfFile = {"ModularBinExpo"}
-            />
+                <br />
+                <br />
+
+                <h6><b>Check in the exponent is odd or even</b></h6>
+
+                To do this just do exponent & 1
+                <br />
+                    
+                For example the number \(000101101010\) if the exponent 
+                was odd then the last digit will be 1, so exponent & 1
+                give true only if the last digit was 1.
+
+                <ShowCode 
+                    Data       = {filesData}
+                    fileName   = {"BinaryExponentiation.cpp"}
+                    partOfFile = {"BinExpo"}
+                />
+                <br />
+
+            <h5><b>Modular Exponentation</b></h5>
+
+                This is to do \( {"base^{exponent}\\mod n"}\)
+
+                <ShowCode 
+                    Data       = {filesData}
+                    fileName   = {"BinaryExponentiation.cpp"}
+                    partOfFile = {"ModularBinExpo"}
+                />
 
         </div>
     )
