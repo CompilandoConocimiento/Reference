ce3d7cd59df834bbc7c851471eb9bf6e4aba7783
Oscar Andrés Rosas <soyoscarrh@gmail.com>
2019-02-13 21:16:55 -0800

    Update to add Operators chapters

--- Books/IntroToCompetitive/examples.cpp
+++ Books/IntroToCompetitive/examples.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
 int main() {
-
-    std::vector<int> someIntegers {1, 2, 3, 4, 20, 5};
-    std::size_t numberOfElements = {someIntegers.size()};
-
-    cout << numberOfElements << endl;
-
     return 0;
 }
