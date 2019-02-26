e0d5cc0e0fe7e98346e0932946cd7d8e13c8c965
Oscar Andrés Rosas <soyoscarrh@gmail.com>
2019-02-25 23:29:43 -0800

    Add prettierrc

--- Webpage/Code/App/App.tsx
+++ Webpage/Code/App/App.tsx
@@ -1,38 +1,41 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { HashRouter, Switch, Route } from 'react-router-dom'
+import { HashRouter, Switch, Route } from "react-router-dom"
 
-import Header           from "./Header"
-import Footer           from "./Footer"
-import Home             from "./Home"
+import Header from "./Header"
+import Footer from "./Footer"
+import Home from "./Home"
 import AlgorithmsPicker from "./AlgorithmsPicker"
 
 /**
-   * This is a wrapper of all the app, the header, the footer and
-   * the content.
-   */
+ * This is a wrapper of all the app, the header, the footer and
+ * the content.
+ */
 const App: React.FunctionComponent = () => (
-	<React.Fragment>
-		<header>
-			<Header />
-			<br />
-		</header>
+  <React.Fragment>
+    <header>
+      <Header />
+      <br />
+    </header>
 
-		<main>
-			<Switch>
-				<Route exact path='/' component={Home}/>
-				<Route path='/Topic/:TopicLink'
-					render = { 
-						props => <AlgorithmsPicker TopicLink={props.match.params.TopicLink} /> 
-					}
-				/>
-				<Route path='/:SomethingElse' component={Home}/>
-			</Switch>
-		</main>
+    <main>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route
+          path="/Topic/:TopicLink"
+          render={props => <AlgorithmsPicker TopicLink={props.match.params.TopicLink} />}
+        />
+        <Route path="/:SomethingElse" component={Home} />
+      </Switch>
+    </main>
 
-		<Footer />
-		
-	</React.Fragment>
+    <Footer />
+  </React.Fragment>
 )
 
-ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('ReactApp'))
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("ReactApp")
+)
