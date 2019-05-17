1dc5305927ec07a471589feee2c24484c26aff04
Oscar Rosas <soyoscarrh@gmail.com>
2019-05-16 21:17:31 -0500

    Update

--- Books/IntroToCompetitive/examples.cpp
+++ Books/IntroToCompetitive/examples.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    auto x = "hi";
+    vector<int> v {1, 2, 3, 4, 12, 121212, 23, 2, -3};
+    std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; });
+    for_each(v.begin(), v.end(), [](int a) { cout << a << endl; });
     return 0;
 }
