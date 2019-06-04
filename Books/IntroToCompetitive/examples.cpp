7f7551220862646091366b32d3954948878bf349
Oscar Rosas <soyoscarrh@gmail.com>
2019-06-04 18:32:46 -0500

    Update reference

--- Books/IntroToCompetitive/examples.cpp
+++ Books/IntroToCompetitive/examples.cpp
@@ -2,10 +2,49 @@
 #include <vector>
 
 using namespace std;
-vector<int> days ();  
 
+template <class T>  
+inline void getNumberFast(T &result) {                   
+  T number {};                                                      
+  bool sign {};
+
+  char currentDigit {static_cast<char>(getchar_unlocked())};
+  
+  while(currentDigit < '0' or currentDigit > '9') {
+    currentDigit = getchar_unlocked(); 
+    if (currentDigit == '-') sign = true;                     
+  }
+
+  while ('0' <= currentDigit and currentDigit <= '9') {          
+    number = (number << 3) + (number << 1) + (currentDigit - '0');  
+    currentDigit = getchar_unlocked();                             
+  }
+
+  if (sign) result = -number;
+  else result = number;
+}
 
 int main() {
-  vector<int> x = {};
+  int numberOfElements;
+  getNumberFast(numberOfElements);
+
+  cout << numberOfElements << endl;
+
+  std::vector<int> elements {};
+  elements.resize(numberOfElements);
+  cout << elements.size() << endl;
+
+  for (size_t i {}; i < numberOfElements; ++i) {
+    getNumberFast(elements[i]);
+    cout << elements[i] << endl;
+  }
+
+  cout << elements.size() << endl;
+
+  for (auto x : elements) {
+    cout << 2 * x << endl;
+  }
+
+  return 0;
 }
 
