265b3643d69dfc2afa6b007be29782e394173fe6
Oscar Rosas <soyoscarrh@gmail.com>
2019-06-10 01:37:54 -0500

    Update adding clang format

--- Books/IntroToCompetitive/examples.cpp
+++ Books/IntroToCompetitive/examples.cpp
@@ -4,9 +4,11 @@
 using namespace std;
 
 int main() {
-  int number { 0b00000110 };
-  int number1 = number >> 1; // 0b00001100
-  int number2 = number >> 3; // 0b00110000
+  int number {0b00000110};
+  int* a;
+
+  int number1 = number >> 1;  // 0b00001100
+  int number2 = number >> 3;  // 0b00110000
   if (number1 == 0b00000011) cout << "Aaa" << endl;
   if (number2 == 0b00000000) cout << "Aaa2" << endl;
 
