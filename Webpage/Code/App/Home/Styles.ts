8eec95f2064dd8103fc992f188d81ce531b69020
Oscar Rosas <soyoscarrh@gmail.com>
2019-06-16 01:36:03 -0500

    fuck hover

--- Webpage/Code/App/Home/Styles.ts
+++ Webpage/Code/App/Home/Styles.ts
@@ -5,15 +5,18 @@ const useStyles = makeStyles(() =>
     Card: {
       cursor: "pointer",
       borderRadius: "1.2rem",
-      padding: "1.2rem"
+      padding: "1.2rem",
+      "&:hover": {
+        boxShadow: "0 4px 6px rgba(0,0,0,0.16), 0 4px 6px rgba(0,0,0,0.23);",
+      },
     },
     Text: {
-      color: "white"
+      color: "white",
     },
     Divider: {
       borderTop: "4px solid rgba(255, 255, 255, 0.3)",
       marginBottom: "1rem",
-    }
+    },
   })
 )
 
