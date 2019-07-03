e04c9fd58f8d89b88f312cdca4ab245391b48cf0
Oscar Rosas <soyoscarrh@gmail.com>
2019-07-02 19:31:35 -0500

    Fix the reference

--- Code/NumberTheory/BinaryExponentiation.cpp
+++ Code/NumberTheory/BinaryExponentiation.cpp
@@ -1,54 +1,56 @@
 template <typename integer, typename unsignedInteger>
-integer RecursiveBinaryExp (integer x, unsignedInteger n) {
-    if (n == 0) return 1;                                         
-    if (n == 1) return x;                                           
+auto binaryExponentation(integer x, unsignedInteger n) -> integer {
+  if (n == 0) return 1;
+  if (n == 1) return x;
 
-    if (n % 2 == 0) return RecursiveBinaryExp(x * x,  n / 2);           
-    else return (x * RecursiveBinaryExp(x * x, (n-1) / 2));  
+  if (n % 2 == 0) return binaryExponentation(x * x, n / 2);
+  return (x * binaryExponentation(x * x, (n - 1) / 2));
 }
 
 template <typename integer, typename unsignedInteger>
-integer SimpleBinaryExponentation (integer base, unsignedInteger exponent) {                  
-    integer solution {1};                                               
+auto binaryExponentation(integer base, unsignedInteger exponent) -> integer {
+  auto solution = integer {1};
 
-    while (exponent > 0) {
-        if (exponent % 2) {
-            solution = base * solution;
-            base = base * base;                            
-        }                                
-        else base = base * base;                            
-
-        exponent = exponent / 2;                    
+  while (exponent > 0) {
+    if (exponent & 1) {
+      solution = base * solution;
+      base = base * base;
+    } else {
+      base = base * base;
     }
 
-    return solution;                              
+    exponent = exponent / 2;
+  }
+
+  return solution;
 }
 
 template <typename integer, typename unsignedInteger>
-integer BinaryExponentation (integer base, unsignedInteger exponent) {                  
-    integer solution {1};                                               
+auto binaryExponentation(integer base, unsignedInteger exponent) -> integer {
+  auto solution = integer {1};
 
-    while (exponent > 0) {                                
-        if (exponent & 1) solution = base * solution; 
+  while (exponent > 0) {
+    if (exponent & 1) solution = base * solution;
 
-        base = base * base;                            
-        exponent = exponent >> 1;                    
-    }
+    base = base * base;
+    exponent = exponent >> 1;
+  }
 
-    return solution;                              
+  return solution;
 }
 
 template <typename integer, typename uinteger>
-integer ModularBinaryExponentation(integer base, uinteger exponent, uinteger n) {     
-    integer solution {1};                                               
-    base = base % n;                                            
+auto modularBinaryExponentation(integer base, uinteger exponent, uinteger n)
+    -> integer {
+  integer solution {1};
+  base = base % n;
 
-    while (exponent > 0) {                                          
-        if (exponent & 1) solution = (base * solution) % n;         
-        
-        base = (base * base) % n;                                   
-        exponent = exponent >> 1;                                   
-    }
+  while (exponent > 0) {
+    if (exponent & 1) solution = (base * solution) % n;
 
-    return solution;                                                
+    base = (base * base) % n;
+    exponent = exponent >> 1;
+  }
+
+  return solution;
 }
