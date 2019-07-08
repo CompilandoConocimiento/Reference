51bf3b126419168d70d63c90bbefcde21c38fd69
Oscar Rosas <soyoscarrh@gmail.com>
2019-07-07 20:59:44 -0500

    Fix typos

--- Books/IntroToCompetitive/examples.cpp
+++ Books/IntroToCompetitive/examples.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 int main() {
-  int number {0b00000110};
-  int* a;
+  auto someMap = map<int, string> {
+      {15, "fifteen"},
+      {0, "null"},
+      {8, "eight"},
+  };
 
-  int number1 = number >> 1;  // 0b00001100
-  int number2 = number >> 3;  // 0b00110000
-  if (number1 == 0b00000011) cout << "Aaa" << endl;
-  if (number2 == 0b00000000) cout << "Aaa2" << endl;
+  for (const auto& p : someMap) cout << p.first << '\n';
 
   return 0;
 }
--- Code/ToDo.md
+++
@@ -1,7 +0,0 @@
-* Trie
-* CreateBTSfromSorted
-* Numerical Analysis algorithms
-* XOR and propierties a¡in Foobar
-* Fast fibonacci numbers using matrix
-* Segment tree
-* Check the new template
