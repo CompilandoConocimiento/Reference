8a09f08a28291852708b5d19a8a2625bbd9a09f3
SoyOscarRH <soyoscarrh@gmail.com>
2019-07-11 19:17:47 -0500

    Add i /o

--- Books/IntroToCompetitive/examples.cpp
+++ Books/IntroToCompetitive/examples.cpp
@@ -1,33 +1,12 @@
-#include <array>
+#include <iomanip>
 #include <iostream>
-#include <vector>
-
-template <typename container>
-auto findSecond(const container& data) {
-  // We need 2 elements to find the second min :c
-  assert(data.size() > 1);
-
-  auto minimum = data[0], almostMin = data[1];
-  for (const auto current : data) {
-    // If we have found a new min move everything
-    if (current < minimum) {
-      almostMin = minimum;
-      minimum = current;
-      // Ok, not new min, but maybe we found a new 2 min
-    } else if (current < almostMin)
-      almostMin = current;
-  }
-  return almostMin;
-};
 
 auto main() -> int {
-  using namespace std;
-
-  vector<int> data1 {1, 4, 1};
-  cout << findSecond(data1) << endl;  // 1
-
-  array<int, 5> data2 {1, 4, -1, 3, -5};
-  cout << findSecond(data2) << endl;  // -1
-
+  auto f = 301.14159;
+  std::cout << std::setprecision(5) << f << '\n';
+  std::cout << std::setprecision(9) << f << '\n';
+  std::cout << std::fixed;
+  std::cout << std::setprecision(5) << f << '\n';
+  std::cout << std::setprecision(9) << f << '\n';
   return 0;
 }
